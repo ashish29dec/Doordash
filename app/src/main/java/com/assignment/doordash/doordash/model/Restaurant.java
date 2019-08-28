e634606130cc3bb69118bc0061e5805f91a4704a
ashish29dec <ashish_29_dec@yahoo.com>
2019-08-27 22:49:31 -0700

    Add files via upload

---
+++ app/src/main/java/com/assignment/doordash/doordash/model/Restaurant.java
@@ -0,0 +1,9 @@
+package com.assignment.doordash.doordash.model;
+
+public class Restaurant {
+    public int id;
+    public String name;
+    public String description;
+    public String cover_img_url;
+    public String status;
+}
