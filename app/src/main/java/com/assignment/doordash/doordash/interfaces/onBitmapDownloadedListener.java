123ca3a523a18c572ae648ed672951060866e718
ashish29dec <ashish_29_dec@yahoo.com>
2019-08-27 22:48:51 -0700

    Add files via upload

---
+++ app/src/main/java/com/assignment/doordash/doordash/interfaces/onBitmapDownloadedListener.java
@@ -0,0 +1,7 @@
+package com.assignment.doordash.doordash.interfaces;
+
+import android.graphics.Bitmap;
+
+public interface onBitmapDownloadedListener {
+    public void onBitmapDownloaded(String url, Bitmap bitmap);
+}
